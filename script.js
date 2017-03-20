11e1ca055fcee7890b0d8a3aa40ed66df6083b4a
alexchanwk <alexchanwk@gmail.com>
2017-03-20 15:14:26 +0800

    fix description

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ function initMap() {
     });
 
     $('div#container1').addClass("inFocus");
-    $('#contents').append("<div class='space-at-the-bottom'><a href='#space-at-the-top'><i class='fa fa-chevron-up'></i></br><small>Top</small></a></div>");
+    $('#contents').append("<div id='to_top' class='space-at-the-bottom'><a href='#space-at-the-top'><i class='fa fa-chevron-up'></i></br><small>Top</small></a></div>");
     map.fitBounds(geojson.getBounds());
     geojson.addTo(map);
   });
@@ -153,6 +153,7 @@ function initMap() {
 function resetMap()
 {
 	map.fitBounds(geojson.getBounds());
+	$('to_top').click();
 }
 
 initMap();
