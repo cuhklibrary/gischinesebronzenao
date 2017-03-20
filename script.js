a00aec9097632059a4187679c08f0eb2fbf17812
cuhklibrary <alexchan@lib.cuhk.edu.hk>
2017-03-21 02:17:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function initMap() {
 //  }).addTo(map);
 	
 var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+	maxZoom: 18,
 }).addTo(map);
 
   // This loads the GeoJSON map data file from a local folder
