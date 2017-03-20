6c58603c371f5a4e6e786c3d04fb69122224bca6
cuhklibrary <alexchan@lib.cuhk.edu.hk>
2017-03-21 02:12:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,18 +23,8 @@ function initMap() {
 //    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 //  }).addTo(map);
 	
-var HERE_hybridDay = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
-	attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
-	subdomains: '1234',
-	mapID: 'newest',
-	app_id: '<your app_id>',
-	app_code: '<your app_code>',
-	base: 'aerial',
-	maxZoom: 20,
-	type: 'maptile',
-	language: 'eng',
-	format: 'png8',
-	size: '256'
+var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
   // This loads the GeoJSON map data file from a local folder
