3d1839de3777d04e9d9bda245d99981053879489
alexchanwk <alexchanwk@gmail.com>
2017-03-20 15:50:38 +0800

    fix description

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function initMap() {
             areaTop += $('div#container' + i).height() + imageContainerMargin;
           }
 
-          areaBottom = areaTop + $('div#container' + feature.properties['id']).height() - 50;
+          areaBottom = areaTop + $('div#container' + feature.properties['id']).height();
 
           $('div#contents').scroll(function() {
             if ($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
