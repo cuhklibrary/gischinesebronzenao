83e10fa213c2938ba7f244d9e4fa3d8749d59aa2
alexchanwk <alexchanwk@gmail.com>
2017-03-23 15:43:57 +0800

    Fixed GCJ coordinates

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function initMap() {
 	
 var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-	maxZoom: 18,
+	maxZoom: 17,
 }).addTo(map);
 
   // This loads the GeoJSON map data file from a local folder
